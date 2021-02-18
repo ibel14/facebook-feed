b47b03702e787055274ac350b06859efa82fb052
ponaehali <iambelousov@gmail.com>
2021-02-18 23:59:17 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <h1>Facebook feed</h1>,
+  document.getElementById('root')
+);
+
+export default App;

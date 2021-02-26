bea2d75156b21ca890ac960f121526f8cb241438
ponaehali <iambelousov@gmail.com>
2021-02-26 23:10:14 +0200

    work with a feed

--- src/components/feed-item/feed-item.js
+++ src/components/feed-item/feed-item.js
@@ -1,18 +1,56 @@
 import React from 'react';
+
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 import './feed-item.css';
 
-const FeedItem = () => {
+const FeedItem = (props) => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+    setAnchorEl(null);
+    };
+
     return (
         <li className="app-item flex-direction-column">
+            <div>
+                <MoreHorizIcon aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}/>
+                <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                >
+                <MenuItem onClick={handleClose}>Save</MenuItem>
+                <MenuItem onClick={handleClose}>Hide this post</MenuItem>
+                <MenuItem onClick={handleClose}>Complain</MenuItem>
+            </Menu>
+            </div>
+
+                <div className="user">User</div>
                 <span className="app-item-label">
-                    Facebook post
+                    {props.label}
                 </span>
+                <img className="feed-img" src='url' alt='img'></img>
                 <div className="flex-direction-column">
                     <ThumbUpAltIcon type="button">
                         <i className=""></i>
                     </ThumbUpAltIcon>
+                    <p className="share">
+                        Поделились: {'3'}
+                    </p>
+                    <p className="comments">
+                        Комментарии: {'6'}
+                    </p>
                 </div>
         </li>
     )
